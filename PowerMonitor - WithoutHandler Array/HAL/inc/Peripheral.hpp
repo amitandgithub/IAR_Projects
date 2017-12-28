73d3ca04485fbf5574913b7654a865c57326efbb
amitandgithub <34563851+amitandgithub@users.noreply.github.com>
2017-12-28 17:16:50 +0800

    Added more generic APIs to Peripheral Class

--- PowerMonitor - WithoutHandler Array/HAL/inc/Peripheral.hpp
+++ PowerMonitor - WithoutHandler Array/HAL/inc/Peripheral.hpp
@@ -22,6 +22,14 @@ protected:
 	Peripheral(){};
 	virtual~Peripheral(){}
 	virtual bool HwInit() = 0;
+	virtual bool HwConfig(void* pConfigStruct);
+	virtual void HwEnable();
+	virtual void HwDisable();
+	virtual uint32_t HwGetFlagStatus(uint32_t aFlag );
+	virtual bool HwConfigInterrupt(uint32_t InterruptNo);
+	virtual bool HwEnableInterrupt(uint32_t InterruptNo);
+	virtual bool HwDisableInterrupt(uint32_t InterruptNo);
+	virtual bool HwClearInterrupt(uint32_t InterruptNo);
 	inline ClockManager*     GetClockManager()    { return ClockManager::GetInstance();}
 	inline PortManager*      GetPortManager ()    { return PortManager::GetInstance();}
 	inline InterruptManager* GetInterruptManager(){ return InterruptManager::GetInstance();}
