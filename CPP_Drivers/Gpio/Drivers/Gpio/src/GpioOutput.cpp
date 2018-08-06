0a132d42c7cd81261fafeca1bb3852eefed2c5d2
amitandgithub <amit4u.com@gmail.com>
2018-08-06 22:21:56 +0530

    some edits

--- CPP_Drivers/Gpio/Drivers/Gpio/src/GpioOutput.cpp
+++ CPP_Drivers/Gpio/Drivers/Gpio/src/GpioOutput.cpp
@@ -15,31 +15,31 @@ GpioOutput::GpioOutput(PORT_t Port, PIN_t Pin)
 	m_Port = Port;
 	m_Pin  = Pin;
 	m_Mode = OUTPUT_PP;
-        m_Speed = HIGH;
+    m_Speed = HIGH;
 }
 GpioOutput::GpioOutput(PORT_t Port, PIN_t Pin, MODE_t aMODE)
 {
 	m_Port = Port;
 	m_Pin  = Pin;
 	m_Mode = aMODE;
-        m_Speed = HIGH;
+    m_Speed = HIGH;
 }
 GpioOutput::GpioOutput(PORT_t Port, PIN_t Pin, MODE_t aMODE, SPEED_t SPEED)
 {
 	m_Port = Port;
 	m_Pin  = Pin;
 	m_Mode = aMODE;
-        m_Speed = SPEED;
+    m_Speed = SPEED;
 }
 Status_t GpioOutput::HwInit()
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	const Peripheral_t  PeripheralName =
                                          (m_Port==GPIOA)? APB2Periph_GPIOA :
-				         (m_Port==GPIOB)? APB2Periph_GPIOB :
-				         (m_Port==GPIOC)? APB2Periph_GPIOC :
-				         (m_Port==GPIOD)? APB2Periph_GPIOD :
-				         (m_Port==GPIOE)? APB2Periph_GPIOE : APB2Periph_GPIOA;
+                                         (m_Port==GPIOB)? APB2Periph_GPIOB :
+                                         (m_Port==GPIOC)? APB2Periph_GPIOC :
+                                         (m_Port==GPIOD)? APB2Periph_GPIOD :
+                                         (m_Port==GPIOE)? APB2Periph_GPIOE : APB2Periph_GPIOA;
 
      // Enable the GPIO hardware Clocks
      __HAL_RCC_AFIO_CLK_ENABLE();
@@ -54,6 +54,8 @@ Status_t GpioOutput::HwInit()
          __HAL_RCC_GPIOD_CLK_ENABLE();
      else if(PeripheralName == APB2Periph_GPIOE )
          __HAL_RCC_GPIOE_CLK_ENABLE();
+     else { ;}  // Error }
+         
      
      GPIO_InitStructure.Pin   = m_Pin;
      GPIO_InitStructure.Speed = m_Speed;
